a71da08dcb91bf6de81e05f93ff11d438a8cdb93
xaulosky <pascifuentes@gmail.com>
2023-08-14 21:01:40 -0400

    prueba calendario escritorio

--- src/pages/escritorio/Escritorio.jsx
+++ src/pages/escritorio/Escritorio.jsx
@@ -1,8 +1,32 @@
+import { Box, Card, CardBody, Grid, GridItem, SimpleGrid } from "@chakra-ui/react"
+import Calendario from "../../componets/calendario/Calendario"
 
 const Escritorio = () => {
 
     return (
-        <h1>Escritorio</h1>
+        <Grid templateColumns={"repeat(6, 1fr)"} gap={6}>
+            <GridItem colSpan={
+                {
+                    base: 6,
+                    md: 6,
+                    lg: 2,
+                }
+            }>
+                <Calendario />
+            </GridItem>
+            <GridItem colSpan={
+                {
+                    base: 6,
+                    md: 6,
+                    lg: 4,
+                }
+            }>
+            </GridItem>
+
+
+
+
+        </Grid>
     )
 }
 
